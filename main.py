225e337fa83e779a790ad8090efa200175b1c5f8
Steven Xu <stevenxxiu@gmail.com>
2017-03-17 11:22:49 +1100

    min-freq fix

--- main.py
+++ main.py
@@ -42,9 +42,12 @@ def run_model(
     all_words = Counter(itertools.chain(train_words, val_words, test_words))
     words_index = {}
     for word, count in all_words.items():
+        words_index[word] = len(words_index)
+    train_words_index = {}
+    for word, count in all_words.items():
         if count >= vocab_min_freq:
-            words_index[word] = len(words_index)
-    train_X, train_y = words_to_mat(train_words, step_size, words_index)
+            train_words_index[word] = words_index[word]
+    train_X, train_y = words_to_mat(train_words, step_size, train_words_index)
     val_X, val_y = words_to_mat(val_words, step_size, words_index)
     test_X, test_y = words_to_mat(test_words, step_size, words_index)
 
@@ -76,6 +79,7 @@ def run_model(
     # functions
     train = theano.function([input_var, target_values, lr], cost, updates=updates, on_unused_input='ignore')
     compute_cost = theano.function([input_var, target_values], cost)
+    pred = theano.function([input_var], network_output)
 
     def all_cost(X, y):
         total_cost = 0
